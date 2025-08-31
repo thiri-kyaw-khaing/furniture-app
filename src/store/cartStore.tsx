ffee9771a6244a24160bbbbb93c73019a91ea014
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-08-31 23:54:09 +0700

    cartstore finish

--- src/store/cartStore.tsx
+++ src/store/cartStore.tsx
@@ -23,36 +23,49 @@ interface CartAction {
   clearCart: () => void;
 }
 
-const initialState:CartState{
-    carts:[]
-}
+const initialState: CartState = {
+  carts: [],
+};
 
-export const useCartStore =create<CartState & CartAction> () (
-    persist(
-        immer((set,get)=>({
-...initialState,
-            getTotalItem:()=>{
-                const {carts} = get();
-                return carts.reduce((total,product)=> total+ product.quantity,0);
-            },
-            getTotalPrice:()=>{
-                const {carts} =get();
-                return carts.reduce((total,product)=>total + product.price * product.quantity,0);
-            },
-            addItem:(item)=>set((state)=>{
-                const existingItem=state.carts.find((i)=>i.id===item.id);
-                if(existingItem){
-                    existingItem.quantity=item.quantity||1;
-                }else state.carts.push({...item,quantity:item.quantity||1});//...item=id,name,quantity,price,image
-                //state.carts.push(item)
-            }),
-            updateItem:(id,quantity)=>set((state)=>{
-                
-            })
-        })),
-        {
-            name:"cartStorage",
-            storage:createJSONStorage(()=>localStorage)
-        }
-    )
-)
+export const useCartStore = create<CartState & CartAction>()(
+  persist(
+    immer((set, get) => ({
+      ...initialState,
+      getTotalItem: () => {
+        const { carts } = get();
+        return carts.reduce((total, product) => total + product.quantity, 0);
+      },
+      getTotalPrice: () => {
+        const { carts } = get();
+        return carts.reduce(
+          (total, product) => total + product.price * product.quantity,
+          0
+        );
+      },
+      addItem: (item) =>
+        set((state) => {
+          const existingItem = state.carts.find((i) => i.id === item.id);
+          if (existingItem) {
+            existingItem.quantity = item.quantity || 1;
+          } else state.carts.push({ ...item, quantity: item.quantity || 1 }); //...item=id,name,quantity,price,image
+          //state.carts.push(item)
+        }),
+      updateItem: (id, quantity) =>
+        set((state) => {
+          const item = state.carts.find((item) => item.id === id);
+          if (item) {
+            item.quantity = quantity;
+          }
+        }),
+      removeItem: (id) =>
+        set((state) => {
+          state.carts = state.carts.filter((item) => item.id !== id);
+        }),
+      clearCart: () => set(initialState),
+    })),
+    {
+      name: "cartStorage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
