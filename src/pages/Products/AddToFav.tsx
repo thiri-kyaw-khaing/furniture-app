096f27b32d44d42bf0808eb11b0f1e6605d75e3f
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-09-03 22:40:00 +0700

    fav icon fix

--- src/pages/Products/AddToFav.tsx
+++ src/pages/Products/AddToFav.tsx
@@ -1,17 +1,28 @@
-import {cn} from "@/lib/utils.ts";
-import {Button,type ButtonProps} from "@/components/ui/button.tsx";
-import {HeartIcon} from "lucide-react";
+import { cn } from "@/lib/utils.ts";
+import { Button, type ButtonProps } from "@/components/ui/button.tsx";
+import { HeartIcon } from "lucide-react";
+import { HeartFilledIcon } from "@radix-ui/react-icons";
 
-
-interface FavProps extends ButtonProps{
-    productId:string;
-    rating:number;
+interface FavProps extends ButtonProps {
+  productId: string;
+  rating: number;
 }
 
-export default function AddtoFav({productId,rating,className,...props}:FavProps){
-    return(
-    <Button variant={"secondary"} size={"icon"} className={cn("size-8 shrink-0",className)} {...props}>
-        <HeartIcon className="size-4"/>
+export default function AddtoFav({
+  productId,
+  rating,
+  className,
+  ...props
+}: FavProps) {
+  return (
+    <Button
+      variant={"secondary"}
+      size={"icon"}
+      className={cn("size-8 shrink-0", className)}
+      {...props}
+    >
+      {/* <HeartIcon className="size-4"/> */}
+      <HeartFilledIcon className="size-4 text-red-500" />
     </Button>
-    )
+  );
 }
