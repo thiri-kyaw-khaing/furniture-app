28f48a2eedbb17a04e60a60fbb0c06127cca089a
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-09-03 23:27:46 +0700

    useFetcher

--- src/pages/Products/AddToFav.tsx
+++ src/pages/Products/AddToFav.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils.ts";
 import { Button, type ButtonProps } from "@/components/ui/button.tsx";
 import { HeartIcon } from "lucide-react";
 import { HeartFilledIcon } from "@radix-ui/react-icons";
+import { useFetcher } from "react-router-dom";
 
 interface FavProps extends ButtonProps {
   productId: string;
@@ -16,19 +17,24 @@ export default function AddtoFav({
   className,
   ...props
 }: FavProps) {
+  const fetcher = useFetcher({ key: `product:${productId}` });
   return (
-    <Button
-      variant={"secondary"}
-      size={"icon"}
-      className={cn("size-8 shrink-0", className)}
-      {...props}
-    >
-      {isFavourite ? (
-        <HeartFilledIcon className="size-4 text-red-500" />
-      ) : (
-        <HeartIcon className="size-4 text-red-500" />
-      )}
-      {/* <HeartIcon className="size-4"/> */}
-    </Button>
+    <fetcher.Form method="POST">
+      <Button
+        variant={"secondary"}
+        size={"icon"}
+        name="favourite"
+        value={isFavourite ? "false" : "true"}
+        className={cn("size-8 shrink-0", className)}
+        {...props}
+      >
+        {isFavourite ? (
+          <HeartFilledIcon className="size-4 text-red-500" />
+        ) : (
+          <HeartIcon className="size-4 text-red-500" />
+        )}
+        {/* <HeartIcon className="size-4"/> */}
+      </Button>
+    </fetcher.Form>
   );
 }
